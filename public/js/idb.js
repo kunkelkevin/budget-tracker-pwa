bfb2d95dd3529ed9115b5d9ed5f69016e2683410
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-07-22 20:09:56 -0500

    fixed call to indexDB on returning online.

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,86 +1,86 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open("budget_tracker", 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function(event) {
-    // save a reference to the database 
-    const db = event.target.result;
-    // create an object store (table) called `budget`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('budget', { autoIncrement: true });
-  };
+request.onupgradeneeded = function (event) {
+  // save a reference to the database
+  const db = event.target.result;
+  // create an object store (table) called `budget`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("budget", { autoIncrement: true });
+};
 
-  // upon a successful 
-request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-    db = event.target.result;
-  
-    // check if app is online, if yes run updateBudget() function to send all local db data to api
-    if (navigator.onLine) {
-      // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadPizza();
-    }
-  };
-  
-  request.onerror = function(event) {
-    // log error here
-    console.log(event.target.errorCode);
-  };
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
 
-  // This function will be executed if we attempt to submit a change to the budget and there's no internet connection
-function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['budget'], 'readwrite');
-  
-    // access the object store for `budget`
-    const budgetObjectStore = transaction.objectStore('budget');
-  
-    // add record to your store with add method
-    budgetObjectStore.add(record);
+  // check if app is online, if yes run updateBudget() function to send all local db data to api
+  if (navigator.onLine) {
+    updateBudget();
   }
+};
 
-  function updateBudget() {
-    // open a transaction on your db
-    const transaction = db.transaction(['budget'], 'readwrite');
-  
-    // access your object store
-    const budgetObjectStore = transaction.objectStore('budget');
-  
-    // get all records from store and set to a variable
-    const getAll = budgetObjectStore.getAll();
-  
-    // upon a successful .getAll() execution, run this function
-getAll.onsuccess = function() {
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a change to the budget and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["budget"], "readwrite");
+
+  // access the object store for `budget`
+  const budgetObjectStore = transaction.objectStore("budget");
+
+  // add record to your store with add method
+  budgetObjectStore.add(record);
+}
+
+function updateBudget() {
+  // open a transaction on your db
+  const transaction = db.transaction(["budget"], "readwrite");
+
+  // access your object store
+  const budgetObjectStore = transaction.objectStore("budget");
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+  console.log(getAll);
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/transaction', {
-        method: 'POST',
+      fetch("/api/transaction", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
       })
-        .then(response => response.json())
-        .then(serverResponse => {
+        .then((response) => response.json())
+        .then((serverResponse) => {
           if (serverResponse.message) {
             throw new Error(serverResponse);
           }
           // open one more transaction
-          const transaction = db.transaction(['budget'], 'readwrite');
+          const transaction = db.transaction(["budget"], "readwrite");
           // access the budget object store
-          const pizzaObjectStore = transaction.objectStore('budget');
+          const budgetObjectStore = transaction.objectStore("budget");
           // clear all items in your store
-          pizzaObjectStore.clear();
+          budgetObjectStore.clear();
 
-          alert('All budget items have been submitted!');
+          window.location.reload();
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
     }
   };
-  }
+}
 
-  window.addEventListener('online', updateBudget);
+window.addEventListener("online", updateBudget);
